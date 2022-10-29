14a5760a26a1d9ca34004215c786cb4333e50d1a
Random <78389553+Sam772@users.noreply.github.com>
2022-10-29 12:04:26 +0100

    More stuff

--- Assets/Scripts/Managers/CharacterManager.cs
+++ Assets/Scripts/Managers/CharacterManager.cs
@@ -1,9 +1,5 @@
 using UnityEngine;
 
-/// <summary>
-/// An example of a scene-specific manager grabbing resources from the resource system
-/// Scene-specific managers are things like grid managers, unit managers, environment managers etc
-/// </summary>
 public class CharacterManager : StaticInstance<CharacterManager> {
 
     [SerializeField] private Player _player;
@@ -13,9 +9,9 @@ public class CharacterManager : StaticInstance<CharacterManager> {
         SpawnCharacter(_player, new Vector3(3f, 3f, 3f));
     }
 
-    void SpawnCharacter(Player t, Vector3 pos) {
-        var player = _player;
+    void SpawnCharacter(Player player, Vector3 pos) {
+        player = _player;
 
-        var spawned = Instantiate(_player, pos, Quaternion.identity, transform);
+        Instantiate(_player, pos, Quaternion.identity, transform);
     }
 }
